b47a5b34c72cf63b0445f7b8eeba8d678fd3bdb7
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-14 22:28:57 +0530

    rebase dev

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,14 +1,23 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { NavBar } from "../../components/navbar/NavBar";
 import "./homepage.css";
 import { NavLink } from "react-router-dom";
 import { AddPostModal } from "../../modals";
+import { usePosts, getAllPosts } from "../../features/posts/postSlice";
+import { useDispatch } from "react-redux";
 
 export function HomePage() {
   const [showModal, setShowModal] = useState(false);
   const addPostShowModal = () => {
     setShowModal(!showModal);
   };
+  const { posts, isLoading } = usePosts();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+  console.log(posts);
+
   return (
     <div className="homepage">
       <NavBar />
@@ -47,40 +56,53 @@ export function HomePage() {
           )}
 
           <div className="posts-at-home">
-            <div className="post">
-              <div className="post-user-detail">
-                <img
-                  className="avatar"
-                  src="https://i.pravatar.cc/30"
-                  alt="profile avatar"
-                />
-                <div className="user-name-time">
-                  <div className="profilename">Ishita Keshawani</div>
-                  <div className="username-date">
-                    <div className="username">@ishita1790</div>
-                    <div className="post-date">• 10 May</div>
+            {isLoading ? (
+              <p>Loading..</p>
+            ) : posts.length > 0 ? (
+              posts.map((post) => (
+                <div className="post">
+                  <div className="post-user-detail">
+                    <img
+                      className="avatar"
+                      src="https://i.pravatar.cc/30"
+                      alt="profile avatar"
+                    />
+                    <div className="user-name-time">
+                      <div className="profilename">
+                        {post.firstName} {post.lastName}
+                      </div>
+                      <div className="username-date">
+                        <div className="username">@{post.username}</div>
+                        <div className="post-date">• {post.createdAt}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="post-text">{post.content}</div>
+                  <div className="like-comment-bookmark-section">
+                    <div className="like-comment">
+                      <div className="like-count-section">
+                        <button className="add-post-footer-btn">
+                          <span class="material-symbols-outlined">
+                            favorite
+                          </span>
+                        </button>
+                        <span>{post.likes?.likeCount} </span>
+                      </div>
+
+                      <button className="add-post-footer-btn">
+                        <span class="material-symbols-outlined">comment</span>
+                      </button>
+                    </div>
+                    <button className="add-post-footer-btn">
+                      <span class="material-symbols-outlined">bookmark</span>
+                    </button>
                   </div>
                 </div>
-              </div>
-              <div className="post-text">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores lor repellendus.
-              </div>
-              <div className="like-comment-bookmark-section">
-                <div className="like-comment">
-                  <button className="add-post-footer-btn">
-                    <span class="material-symbols-outlined">favorite</span>
-                  </button>
-                  <button className="add-post-footer-btn">
-                    <span class="material-symbols-outlined">comment</span>
-                  </button>
-                </div>
-                <button className="add-post-footer-btn">
-                  <span class="material-symbols-outlined">bookmark</span>
-                </button>
-              </div>
-            </div>
+              ))
+            ) : (
+              "loading"
+            )}
+
             <div className="post">
               <div className="post-user-detail">
                 <img
